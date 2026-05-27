b696c329cb5f302110f7a6b50247853a74c689c8
Stephane ENGEL <547826+av8r@users.noreply.github.com>
2026-05-27 09:00:24 +0200

    wip

---
+++ lua/config/ft_cfg.lua
@@ -0,0 +1,12 @@
+-- Filetype
+vim.filetype.add({
+    extension = {
+        tf = 'terraform',
+        tfvars = 'terraform',
+        tfstate = 'json',
+        yml = 'yaml.ansible',
+        jinja = 'jinja',
+        jinja2 = 'jinja',
+        j2 = 'jinja',
+    },
+})
